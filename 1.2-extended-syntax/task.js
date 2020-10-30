f04d08c0df100dae7dfcfe67c41313b96a1c4acd
Andrei Surkov <andronsurkov73@gmail.com>
2020-10-30 23:59:06 +0300

    Добавил выполненное домашнее задание №1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,39 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+"use strict";
+
+function getResult(a, b, c) {
+	let x = [];
+	let discriminant = b ** 2 - 4 * a * c;
+	if (discriminant > 0) {
+		x[0] = (-b + discriminant ** 0.5) / (2 * a);
+		x[1] = (-b - discriminant ** 0.5) / (2 * a);
+	} else if (discriminant === 0) {
+		x[0] = -b / (2 * a);
+	}
+	return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+	let len = marks.length;
+	let averageMark = 0;
+	let markSum = 0;
+	let numberMarks = 5;
+	if (len === 0) {
+		return averageMark;
+	} else if (len > numberMarks) {
+		console.log(`Количество оценок больше ${numberMarks}`);
+		marks.splice(numberMarks);
+		len = numberMarks;
+	}
+	for (let mark of marks) {
+		markSum += mark;
+	}
+	averageMark = markSum / len;
+	return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+	let year = new Date().getFullYear();
+	let yearBirthday = dateOfBirthday.getFullYear();
+	let result = ((year - yearBirthday) >= 18) ? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
+	return result;
 }
