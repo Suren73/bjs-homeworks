769b638a762873c8d5d11c4322eeb2fcb8ac6b6a
Andrei Surkov <andronsurkov73@gmail.com>
2020-10-31 21:30:06 +0300

    Оптимизирован код Домашнее задание 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -4,8 +4,8 @@ function getResult(a, b, c) {
 	let x = [];
 	let discriminant = b ** 2 - 4 * a * c;
 	if (discriminant > 0) {
-		x[0] = (-b + discriminant ** 0.5) / (2 * a);
-		x[1] = (-b - discriminant ** 0.5) / (2 * a);
+		x[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
+		x[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
 	} else if (discriminant === 0) {
 		x[0] = -b / (2 * a);
 	}
@@ -13,27 +13,21 @@ function getResult(a, b, c) {
 }
 
 function getAverageMark(marks) {
-	let len = marks.length;
-	let averageMark = 0;
 	let markSum = 0;
 	let numberMarks = 5;
-	if (len === 0) {
-		return averageMark;
-	} else if (len > numberMarks) {
+	if (marks.length === 0) {
+		return 0;
+	} else if (marks.length > numberMarks) {
 		console.log(`Количество оценок больше ${numberMarks}`);
 		marks.splice(numberMarks);
-		len = numberMarks;
 	}
 	for (let mark of marks) {
 		markSum += mark;
 	}
-	averageMark = markSum / len;
-	return averageMark;
+	return markSum / marks.length;
 }
 
 function askDrink(name, dateOfBirthday) {
-	let year = new Date().getFullYear();
-	let yearBirthday = dateOfBirthday.getFullYear();
-	let result = ((year - yearBirthday) >= 18) ? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
+	let result = ((new Date().getFullYear() - dateOfBirthday.getFullYear()) >= 18) ? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
 	return result;
 }
