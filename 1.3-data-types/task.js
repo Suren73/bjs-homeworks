f86bba208c5bc6dd808049200b368462a980a431
Andrei Surkov <andronsurkov73@gmail.com>
2020-11-04 02:00:30 +0300

    Добавил выполненное Домашнее задание №1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,39 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+	if (isNaN(parseInt(percent))) {
+		return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+	}
+	percent = parseFloat(percent);
+
+	if (isNaN(parseInt(contribution))) {
+		return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+	}
+	contribution = parseFloat(contribution);
+
+	if (isNaN(parseInt(amount))) {
+		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+	}
+	amount = parseFloat(amount);
+
+	if (isNaN(date)) {
+		return `Параметр "Дата" содержит неправильное значение "${date}"`;
+	}
+	let n = (date.getFullYear() - new Date().getFullYear()) * 12;
+	let S = amount - contribution;
+	let P = (1 / 12) * (percent / 100);
+	let payment = S * (P + P / (((1 + P) ** n) - 1));
+	let totalAmount = parseFloat((payment * n).toFixed(2));
+
+	return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+	if (!!name) {
+		name = name;
+	} else {
+		name = "Аноним";
+	}
+	let greeting = `Привет, мир! Меня зовут ${name}`;
+	console.log(greeting);
+	return greeting;
 }
