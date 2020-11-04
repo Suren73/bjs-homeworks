fcec745ee5c891b22c9ec3b6b6372f3ab75ee60d
Andrei Surkov <andronsurkov73@gmail.com>
2020-11-05 00:14:45 +0300

    Подправил код Домашнего задания №1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,29 +1,33 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-	if (isNaN(parseInt(percent))) {
+
+	if (!!percent) {
+
+	} else {
 		return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
 	}
-	percent = parseFloat(percent);
 
-	if (isNaN(parseInt(contribution))) {
+	if (isNaN(contribution)) {
 		return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
 	}
-	contribution = parseFloat(contribution);
 
-	if (isNaN(parseInt(amount))) {
+	if (isNaN(amount)) {
 		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
 	}
-	amount = parseFloat(amount);
 
 	if (isNaN(date)) {
 		return `Параметр "Дата" содержит неправильное значение "${date}"`;
 	}
+
 	let n = (date.getFullYear() - new Date().getFullYear()) * 12;
+	if (n <= 0) {
+		return `Параметр "Дата" содержит неправельное значение "${date}"`;
+	}
 	let S = amount - contribution;
-	let P = (1 / 12) * (percent / 100);
+	let P = 1 / 12 * percent / 100;
 	let payment = S * (P + P / (((1 + P) ** n) - 1));
 	let totalAmount = parseFloat((payment * n).toFixed(2));
-
+	console.log(totalAmount);
 	return totalAmount;
 }
 
