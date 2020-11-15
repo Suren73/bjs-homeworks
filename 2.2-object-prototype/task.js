58301828adc8e9bb59efdd2731c4684343b6b3d6
Andrei Surkov <andronsurkov73@gmail.com>
2020-11-15 20:45:18 +0300

    Выполненное Домашнее задание 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,45 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
+
+String.prototype.isPalindrome = function () {
+	let string = this;
+	string = string.toLowerCase().split('');
+
+	for (let i = 0; i < string.length; i++) {
+		if (string[i] === " ") {
+			string.splice(i, 1);
+			i--;
+		}
+	}
+	let str = string.join('');
+	string = string.reverse().join('');
+	return string === str;
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+
+	let markSum = 0;
+	if (marks.length === 0) {
+		return 0;
+	}
+	for (let mark of marks) {
+		markSum += mark;
+	}
+	let average = markSum / marks.length;
+	let averageMark = Math.round(average);
+
+	return averageMark;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+	let now = Date.now();
+
+	let data = new Date(birthday);
+	birthday = +data;
+	let diff = now - birthday;
+	const year = 31536000000;
+	let age = diff / year;
+	let verdict = (age > 18);
+
+	return verdict
 }
